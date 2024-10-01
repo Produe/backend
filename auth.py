b125329d00b12aa6380cdc50dba9862fd01e85e4
CakeLiar <79081062+CakeLiar@users.noreply.github.com>
2024-10-01 17:02:00 +0300

    Add files via upload

--- auth.py
+++ auth.py
@@ -1,18 +1,136 @@
+from flask import Flask, request, jsonify,abort
 import firebase_admin
 from firebase_admin import credentials, firestore
-from flask import Flask, request, jsonify, abort
-import re
 import requests
+import uuid
+import datetime
+from dotenv import load_dotenv
+import os
+import re
 
-valRegEx = r'^[a-zA-Z0-9_-]+$'
 app = Flask(__name__)
+load_dotenv("TOKENS.env")
+valRegEx = r'^[a-zA-Z0-9_-]+$'
 
-cred = credentials.Certificate('Bu1ldbot/produ-5d1cb-firebase-adminsdk-8hzdo-f44bea1278.json')
-firebase_admin.initialize_app(cred)
+from google.oauth2 import service_account
+credentials = service_account.Credentials.from_service_account_file("produ-5d1cb-firebase-adminsdk-8hzdo-f44bea1278.json")
+db = firestore.client(credentials=credentials)
 
-db = firestore.client()
+SLACK_CLIENT_ID = os.getenv("SLACK_CLIENTID")
+SLACK_CLIENT_SECRET = os.getenv("SLACK_CLIENT_SECRET")
+SLACK_REDIRECT_URI = 'https://extraordinary-nasturtium-9fc3f1.netlify.app/dashboard/'
 
 COLLECTION_NAME = 'users'
+
+
+
+@app.route('/auth', methods=['POST'])
+def auth():
+    data = request.json
+    auth_code = data.get('AUTH_CODE')
+    session_id = request.headers.get('Session-ID')
+
+    if auth_code:
+        response = requests.post('https://slack.com/api/oauth.v2.access', data={
+            'code': auth_code,
+            'client_id': SLACK_CLIENT_ID,
+            'client_secret': SLACK_CLIENT_SECRET,
+            'redirect_uri': SLACK_REDIRECT_URI
+        })
+
+        if response.status_code != 200:
+            abort(500)
+
+        tokens = response.json()
+        access_token = tokens.get('access_token')
+        user_id = tokens.get('authed_user', {}).get('id')
+        team_id = tokens.get('team', {}).get('id')
+
+        if not access_token or not user_id or not team_id:
+            return jsonify({'request_state': 500})
+        user_info_response = requests.get('https://slack.com/api/users.info', headers={
+            'Authorization': f'Bearer {access_token}'
+        }, params={'user': user_id})
+
+        if user_info_response.status_code != 200:
+            abort(500)
+
+        user_info = user_info_response.json()
+
+        if not user_info.get('ok'):
+            abort(500)
+        user = user_info['user']
+        name = user.get('profile', {}).get('display_name')
+        name = name.split()[0]
+        users_ref = db.collection(COLLECTION_NAME).document(user_id)
+        user_doc = users_ref.get()
+
+        session_id = str(uuid.uuid4())
+        creation_date = datetime.datetime.now(datetime.timezone.utc)
+
+        if not user_doc.exists():
+            users_ref.set({
+                'clickup_token': None,
+                'github_token': None,
+                'github_admin': None,
+                'github_repo': None,
+                'name': name, 
+                'session_id': session_id,
+                'team_id': team_id
+            })
+        else:
+            users_ref.update({'session_id': session_id, 'team_id': team_id, 'name': name})
+
+        db.collection('sessions').document(session_id).set({
+            'access_token': access_token,
+            'user_id': user_id,
+            'team_id': team_id,
+            'created_at': creation_date
+        })
+        res=""
+        res.headers['Session-ID'] = session_id
+        return res,200
+
+
+    if session_id:
+        doc_ref = db.collection('sessions').document(session_id)
+        doc = doc_ref.get()
+        if doc.exists():
+            session_data = doc.to_dict()
+
+            user_id = session_data.get('user_id')
+            user_ref = db.collection(COLLECTION_NAME).document(user_id)
+            user_doc = user_ref.get()
+
+            if user_doc.exists:
+                user_data = user_doc.to_dict()
+                session_data.update(user_data)
+                return jsonify({
+                "AUTH_CODE": auth_code,
+                "USER_NAME":user_data.get("name"),
+                "GITHUB_STATUS": 1 if user_data.get("github_token") != None else 0, 
+                "CLICKUP_STATUS": 1 if user_data.get("github_token") != None else 0, 
+                "GITLAB_STATUS":1 if user_data.get("gitlab")!= None else 0, 
+                "JIRA_STATUS": 1 if user_data.get("jira") != None else 0
+                })
+        else:
+            abort(401)
+
+    abort(401)
+
+@app.route('/is_authenticated', methods=['POST'])
+def is_authenticated():
+    session_id = request.headers.get('Session-ID')
+    if session_id:
+        session_ref = db.collection('sessions').document(session_id).get()
+        
+        if session_ref.exists:
+            return 200
+        else:
+            abort(401)
+    else:
+        abort(401)
+
 
 
 def validate_github_token(github_token):
@@ -151,6 +269,11 @@ def update_user_data():
                         "CLICKUP_STATUS": 1 if user_data.get("github_token") != None else 0, 
                         "GITLAB_STATUS":1 if user_data.get("gitlab")!= None else 0, 
                         "JIRA_STATUS": 1 if user_data.get("jira") != None else 0}), 200
+
+
+
+
+
 
 
 if __name__ == '__main__':
